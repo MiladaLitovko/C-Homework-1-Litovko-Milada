827990f3e8cc765cf31a01795494a7569ba55de2
litovkomilada <litovkomiada@yandex.ru>
2022-06-24 21:40:35 +0300

    Complete AddTask1

---
+++ AddTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 1. Пользователь вводит число N (N > 0). Программа должна вывести N единиц на экран.
+N = 4 -> 1, 1, 1, 1
+N = 2 -> 1, 1
+*/
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count=1;
+
+while(count <= number)
+    {
+        Console.Write(1 + " ");
+        count++;
+    }
