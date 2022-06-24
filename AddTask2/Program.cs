1367f9bf3e7ed19e695c4c998052998e1373a0f1
litovkomilada <litovkomiada@yandex.ru>
2022-06-24 22:13:00 +0300

    Complete AddTask2

---
+++ AddTask2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2. Даны два числа a, b, такие что a < b. Вывести на экран сколько раз число a поместиться в числе b.
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int zero = 0;
+int count = numberA; 
+
+
+while(count <= numberB)
+{
+    count = count + numberA;
+    zero = zero + 1;
+}
+Console.Write(zero);
