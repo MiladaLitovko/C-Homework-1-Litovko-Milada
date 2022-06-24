5f13b02ce0aff9da49cd956a80ff914bdb53e3c8
litovkomilada <litovkomiada@yandex.ru>
2022-06-24 22:50:40 +0300

    Complete AddTask4

---
+++ AddTask4/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Задача 4. Дано N секунд. Вывести время в формате часы:минуты:секунды.
+N = 72334 -> 20:5:34 
+*/
+
+Console.WriteLine("Введите кол-во секунд");
+int number = Convert.ToInt32(Console.ReadLine());
+int minutes = number / 60;
+int hour = minutes / 60;
+Console.WriteLine($"{hour}:{minutes}:{number}");
